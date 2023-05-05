6aa2bbb253fdd98f403e9d9013349a87d5ca62bd
ammour rachida <ammour76@gmail.com>
2023-05-05 09:13:08 +0200

    footer

--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -6,18 +6,29 @@ use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class CategoryType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-        ->add('statut',ChoiceType::class,[
-            'choices'=>[
-            "Par continent"=> 0,
-            "Par Type de jeu"=> 1,  
-            ],
-        ]);
+        ->add('name',TextType::class,[
+            'label'=> "Nom de la catégorie",
+                'required' => false,
+                'attr' => [
+                    "placeholder" => 'ajouter catégorie',
+                    'class'=>'form-contol',
+                ]
+        ])
+        // ->add('name',ChoiceType::class,[
+        //     'choices'=>[
+        //     "Par continent"=> 0,
+        //     "Par Type de jeu"=> 1,  
+        //     ],
+        // ])
+        ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
--- src/Form/JeuType.php
+++ src/Form/JeuType.php
@@ -3,8 +3,10 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Choice;
 
 class JeuType extends AbstractType
 {
@@ -14,6 +16,13 @@ class JeuType extends AbstractType
             ->add('titre')
             ->add('photo')
             ->add('description')
+            
+            // ->add('category',ChoiceType::class,[
+            //     'choices'=>[
+            //     "Par continent"=> 0,
+            //     "Par Type de jeu"=> 1,  
+            //     ],
+            // ]);
         ;
     }
 
